bd98595cad309c565d4c7ffb19ebd3c3090e344e
ShazX4-dev <bratva.shah88@gmail.com>
2026-05-08 21:15:37 +0900

    feat:python sistem variables &special viral method DUNDER

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-message = "Hello World!"
+# # message = "Hello World!"
+# # print(message)
+# # men gitga yangi python file yukladim
+
+# def getReverse(Salomalekum):
+#     return Salomalekum[::-1]
+
+
+# Dunder: __builtins___,  __init__
+
+message = "Python: xamma narsa objet"
 print(message)
-men gitga yangi python file yukladim
+
+result = type(message)
+print("result:", result)
+
+''' In Python, there are builtin tools:
+(1) Types > int float str list dict(keyword arguments
+(2) Function > print() len() input() type()
+(3)Constants > True False None
+'''
+print(dir(__builtins__))
