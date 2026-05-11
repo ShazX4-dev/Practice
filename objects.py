3329f28cf3c21a41589d5f6e0905c8851299839a
ShazX4-dev <bratva.shah88@gmail.com>
2026-05-11 21:36:59 +0900

    feat: learn python objects

---
+++ objects.py
@@ -0,0 +1,22 @@
+''' OBJECTS
+(1) What is object
+(2) Iterable objects & RANGE
+(3) DICTIONARY
+(4) ERROR HANDLING SYTEM
+'''
+import array  # package/module
+import math  # from math ceil deb yaxlit ceil methodini qulga olsa buladi
+print("====== What is object =======")
+# an object has state and method properties.
+# everything is object in python!
+
+print(type("Hello World!"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigm > Functional Programming & OOP
+# OOP 4 CONCEPTS > Abstraction | Encapsulation | Inheritence | Polimorphism
+result1 = math.ceil(97.7)  # Call
+print("result:", result1)
