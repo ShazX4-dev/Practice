f0a3c84de1d34fbd8529facfa23ff72e7277afa8
ShazX4-dev <bratva.shah88@gmail.com>
2026-05-11 22:11:56 +0900

    feat: learning iterable objects

---
+++ iterable.py
@@ -0,0 +1,29 @@
+print("======== Iterable objects & Range ====")
+# Iterable objects > string dict tuple list range map filter
+
+range_obj = range(3)
+print("range_obj:", range_obj)
+
+for letter in "MIT":
+    print(f"the letter: {letter}")
+
+for ele in range_obj:
+    print(f"the element: {ele}")
+
+
+print("======== DICTIONARY =======")
+# Dictionary is Json Object!
+person = {"name": "Tom", "age": 25, "single": False}
+person_obj = dict(name="Tom", age=25, single=False)
+print(f"the person: {person}")
+print(f"the person_obj: {person_obj}")
+
+# method: get() getni ishlatsak codelar crash bumaydi va dictni ichida hobby bumagani uchun none qiymat qaytadi
+name = person_obj.get("name")
+hobby = person_obj.get("hobby")
+print(f"the name: {name}, hobby: {hobby}")
+
+
+del person_obj["single"]
+for key in person_obj:
+    print(f"the key: {key} => value {person_obj[key]}")
