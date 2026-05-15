866ce64742f3e8b976a81058196d5c004b7a8ed1
ShazX4-dev <bratva.shah88@gmail.com>
2026-05-15 23:39:58 +0900

    feat: MIT Task I

--- train.task.py
+++ train.task.py
@@ -6,3 +6,26 @@ def getHighestIndex(arr):
 
 
 print(getHighestIndex([5, 21, 12, 21, 8]))
+
+
+def majorityElement(nums):
+    # 1. Har bir elementni sanash uchun lug‘at (dict) yaratamiz
+    count = {}
+
+    # 2. Ro‘yxatdagi elementlarni aylantirib chiqamiz
+    for num in nums:
+        # Agar element lug‘atda bo‘lsa, qiymatini +1 qilamiz
+        if num in count:
+            count[num] += 1
+        else:
+            # Agar bo‘lmasa, uni 1 dan boshlab qo‘yamiz
+            count[num] = 1
+
+    # 3. Eng ko‘p takrorlangan elementni topamiz
+    max_element = max(count, key=count.get)
+
+    return max_element
+
+
+# Sinab ko‘ramiz
+print(majorityElement([1, 2, 3, 4, 5, 4, 3, 4]))  # natija: 4
